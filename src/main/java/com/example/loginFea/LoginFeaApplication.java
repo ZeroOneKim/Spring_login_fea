28e051e9a69ca9123457f1fb16b2337fab56a329
ZeroOneKim <Wowwow0124@naver.com>
2022-05-30 01:25:52 +0900

    기본 페이지.

---
+++ src/main/java/com/example/loginFea/LoginFeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.loginFea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginFeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginFeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loginFea/LoginFeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginFea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginFeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
